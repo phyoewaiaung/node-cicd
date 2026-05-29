a20d106838ecbf264585906c117dd2173e3f1a79
phyo <phyo@xenoptics.com>
2026-05-29 06:51:57 +0000

    add health check endpoint

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send("NOW CI/CD is okay");
 });
 
+app.get("/health", (req, res) => {
+  res.json({ status: "ok" });
+});
+
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
